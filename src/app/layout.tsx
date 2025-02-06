d6d4b9a22323c4d05d6813fc6bf4e834b92d474f
iriadda <iriadda@gmail.com>
2025-02-06 13:33:58 +0200

    hw 1 structure folders update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Menu} from "@/components/Menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Menu/>
+        <hr/>
         {children}
       </body>
     </html>
